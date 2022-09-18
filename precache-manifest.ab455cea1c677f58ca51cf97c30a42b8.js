19e9b0ae8aca1636c0e2a35fb5ba64b72d2e3144
Sahaj Arya <sahajarya@Sahajs-MacBook-Air.local>
2022-09-18 21:45:36 +0530

    Updates

---
+++ precache-manifest.ab455cea1c677f58ca51cf97c30a42b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a68edec1dffcee1ed2afa9ac8c0a541",
+    "url": "/try/index.html"
+  },
+  {
+    "revision": "899d166217893d43a378",
+    "url": "/try/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "df923abee399f9950e10",
+    "url": "/try/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "899d166217893d43a378",
+    "url": "/try/static/js/2.1f0794d1.chunk.js"
+  },
+  {
+    "revision": "df923abee399f9950e10",
+    "url": "/try/static/js/main.80e0df98.chunk.js"
+  },
+  {
+    "revision": "e843b5b1585b8fa0eb4f",
+    "url": "/try/static/js/runtime~main.f38f2584.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/try/static/media/logo.9f7e506d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32369cee55ad6d0176ae9bf7128489d4.js"
+  "/try/precache-manifest.ab455cea1c677f58ca51cf97c30a42b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/try/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
