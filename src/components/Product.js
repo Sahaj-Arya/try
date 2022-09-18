503b2e7d895f2c72f45b2dd80d3d44305bf6c43c
Sahaj Arya <sahajarya@Sahajs-MacBook-Air.local>
2022-09-18 21:52:26 +0530

    gfj

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,11 @@ export default class Product extends Component {
                 onClick={() => value.handleDetail(id)}
               >
                 <Link to="/details">
-                  <img src={img} alt="product" className="card-img-top" />
+                  <img
+                    src={{ URL: img }}
+                    alt="product"
+                    className="card-img-top"
+                  />
                 </Link>
                 <button
                   className="cart-btn"
