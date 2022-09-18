7fe80abe2021c1707e4d55d467e7ff22dd74fec1
Sahaj Arya <sahajarya@Sahajs-MacBook-Air.local>
2022-09-18 21:26:31 +0530

    Updates

---
+++ precache-manifest.925b0a65bf270e1906976da011be7f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a9d4689e77b99f52ca0879a5aeed86",
+    "url": "/try/index.html"
+  },
+  {
+    "revision": "899d166217893d43a378",
+    "url": "/try/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "3f6d230d1617b6d45d11",
+    "url": "/try/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "899d166217893d43a378",
+    "url": "/try/static/js/2.1f0794d1.chunk.js"
+  },
+  {
+    "revision": "3f6d230d1617b6d45d11",
+    "url": "/try/static/js/main.6975aa38.chunk.js"
+  },
+  {
+    "revision": "e843b5b1585b8fa0eb4f",
+    "url": "/try/static/js/runtime~main.f38f2584.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/try/static/media/logo.9f7e506d.svg"
+  }
+]);
